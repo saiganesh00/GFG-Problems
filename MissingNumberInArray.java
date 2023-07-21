c4f6bb57636f87f966ae995deed5c3ac59227869
saiganesh00 <140135125+saiganesh00@users.noreply.github.com>
2023-07-21 11:37:37 +0530

    Day-1

--- MissingNumberInArray.java
+++ MissingNumberInArray.java
@@ -1,4 +1,13 @@
+/* 
+    Given an array of size N-1 such that it only contains distinct integers in the range of 1 to N. Find the missing element.
 
+    Example 1:
+
+    Input:
+    N = 5
+    A[] = {1,2,3,5}
+    Output: 4
+*/
 import java.util.*;
 import java.io.*;
 class MissingNumberInArray {
